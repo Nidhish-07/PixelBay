c69b11aa88edd48496cc1779531eff179ac88ae5
r3X <rch246chauhan@gmail.com>
2022-06-23 18:00:03 +0530

    Commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React,{ Fragment } from "react";
+import "./App.css";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Home from "./container/Home";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
