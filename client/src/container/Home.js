04da7db8aa5b8ceb8fa94fb004ba7fce618f879e
r3X <rch246chauhan@gmail.com>
2022-07-07 18:10:03 +0530

    User profile

--- client/src/container/Home.js
+++ client/src/container/Home.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { HiMenu } from "react-icons/hi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { Link, Route, Routes } from "react-router-dom";
+
+import { SideBar, UserProfile } from "../components";
+import { client } from "../client";
+import Pins from "./Pins";
+import logo from "../assets/profile.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [toggleSideBar, setToggleSideBar] = useState(false);
+  const userInfo =
+    localStorage.getItem("user") !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : localStorage.clear();
+
+      useEffect(() => {
+        
+      }, []);
+
+  return (
+    <div className="flex bg-grey-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
+        <SideBar />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <HiMenu
+          fontSize={40}
+          className="cursor-pointer"
+          onClick={() => setToggleSideBar(false)}
+        />
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28" />
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={logo} alt="logo" className="w-28" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
